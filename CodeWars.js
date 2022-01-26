aeb03c6c055c1e8c9cc2e5950713743843fa2297
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-01-26 15:01:55 +0300

    added task

--- CodeWars.js
+++ CodeWars.js
@@ -11,3 +11,15 @@ const doubleEveryOther = (a) => {
     }, []);
 }
 //
+// Time	Cost
+// Up to 1st hour	$30
+// Every subsequent half hour**	$10
+// ** Subsequent charges are calculated by rounding up to nearest half hour.
+function cost (mins) {
+    if(mins < 0 || typeof(mins) !== 'number') return 'Enter correct number'
+    const price35min = 10, price65min = 30;
+    if(mins <= 65){return price65min}
+    if(mins <= 95 && mins > 65 ){return price35min + price65min}
+    if(mins > 95){return (Math.ceil((mins-95) / 30))*price35min+(price35min+price65min)}
+    //litle return 30 + ( mins>65 ?  Math.ceil((mins-65)/30) : 0 )*10
+}
